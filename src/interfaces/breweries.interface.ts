de6f6d610d763d43aeca92e3eb4c0eb23dc04565
Marco Franceschi <franceschi.marco@gmail.com>
2021-05-22 15:34:45 -0400

    Added Brewery interface

---
+++ src/interfaces/breweries.interface.ts
@@ -0,0 +1,20 @@
+export interface Brewery {
+  id: number;
+  obdbId: string;
+  name: string;
+  breweryType: string;
+  street: string;
+  address2: string;
+  address3: string;
+  city: string;
+  state: string;
+  countyProvince: string;
+  postalCode: string;
+  country: string;
+  longitude: string;
+  latitude: string;
+  phone: string;
+  websiteUrl: string;
+  updatedAt: string;
+  createdAt: string;
+}
